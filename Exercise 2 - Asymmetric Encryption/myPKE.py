05975dd5dd5ef1395cd96fddac1c00b151437ffb
Tom <tom.jackson442@hotmail.com>
2021-10-22 13:23:21 +0100

    Finished Exercise 2

--- Exercise 2 - Asymmetric Encryption/myPKE.py
+++ Exercise 2 - Asymmetric Encryption/myPKE.py
@@ -25,18 +25,32 @@ def createM3(m1,m2,N):
 
     for i in range(0,N):
         for j in range(0,N):
-            afterM1 = m1[j]
-            afterM2 = m2[afterM1-1][i]
-            m3[j][afterM2-1] = i+1
+            afterM1 = m1[j]-1
+            afterM2 = m2[afterM1][i]-1
+            m3[afterM2][j] = i+1
     return m3
 
-if(__name__ == '__main__'):
+#if(__name__ == '__main__'):
     N = int(sys.argv[1])
 
-m1 = createM1(N)
-m2 = createM2(N)
+m1 = [4, 1, 3, 5, 2]
+m2 = [[5, 1, 4, 2, 3],[2, 4, 5, 1, 3],[3, 5, 2, 4, 1],[4, 1, 2, 3, 5],[1, 5, 3, 4, 2]]
+N = 5
+
+#m1 = createM1(N)
+#m2 = createM2(N)
 m3 = createM3(m1,m2,N)
 
-print(m1)
-print(m2)
-print(m3)
+#print(m1)
+#print(m2)
+#print(m3)
+
+k = 1
+p = 2
+
+A = m1[k-1]
+print(A)
+B = m2[A-1][p-1]
+print(B)
+C = m3[B-1][k-1]
+print(C)
